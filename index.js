95f31dc783c7b1eedae19c3309e489c75931f3c6
mandy.klingbeil <mandy.klingbeil@gmail.com>
2018-11-28 22:27:23 +0100

    Add word pairs according to Hassenzahl
    (replace placeholder entries with actual survey content)

--- index.js
+++ index.js
@@ -69,7 +69,53 @@ function resize_survey_content(width, height) {
   $("#question_content").css({"width": width + "%", "height" : height + "%"});
 }
 
+function word_pairs() {
+
+  /*
+  Marc Hassenzahl, Annika Wiklund-Engblom,
+  Anette Bengs, Susanne Hägglund & Sarah Diefenbach
+
+  (2015) Experience-Oriented and Product-Oriented Evaluation:
+  Psychological Need Fulfillment, Positive Affect, and Product
+  Perception, International Journal of Human-Computer Interaction,
+  31:8, 530-544, DOI: 10.1080/10447318.2015.1064664
+  */
+
+  var pragmatic_quality = [
+    ["human", "technical"],
+    ["simple", "complicated"],
+    ["practical", "impractical"],
+    ["straightforward", "cumbersome"],
+    ["predictable", "unpredictable"],
+    ["clearly structured", "confusing"],
+    ["manageable", "unruly"],
+  ];
+
+  var hedonic_quality = [
+    ["connective", "isolating"],
+    ["professional", "unprofessional"],
+    ["stylish", "tacky"],
+    ["premium", "cheap"],
+    ["integrating", "alienating"],
+    ["brings me closer to people", "separates me from people"],
+    ["presentable", "unpresentable"],
+    ["inventive", "conventional"],
+    ["creative", "unimaginative"],
+    ["bold", "cautious"],
+    ["innovative", "conservative"],
+    ["captivating", "dull"],
+    ["challenging", "undemanding"],
+    ["novel", "ordinary"]
+  ]
+   return {"pragmatic_quality" : pragmatic_quality,
+           "hedonic_quality" : hedonic_quality}
+}
+
+
 function content_stage_2() {
-  $(".title").text("Please help us improve.")
-  $(".content").html("<ul><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li><li>This is some question...</li></ul>")
+  $(".title").text("Please help us improve by rating us according to the follwing criteria.");
+  fill_survey(word_pairs().pragmatic_quality);
+  //fill_survey_partial(word_pairs().hedonic_quality, 3, 0);
+
+  show_survey_table(true);
 }
