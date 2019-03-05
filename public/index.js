8299b2e2146a5b66bbeed0024f081bee66a10a7e
mandy.klingbeil <mandy.klingbeil@gmail.com>
2019-03-05 22:23:34 +0100

    apply proper formatting to source files

--- public/index.js
+++ public/index.js
@@ -1,3 +1,3 @@
-$(function() {
+$(function () {
     init_survey("my_survey_div");
 });
--- public/lib/surveytool/surveytool.js
+++ public/lib/surveytool/surveytool.js
@@ -1,10 +1,10 @@
 function show_survey_table(show) {
-    if (show === true) {
-      $("#survey_area").removeClass("hidden");
-    }
-    else {
-      $("#survey_area").addClass("hidden");
-    }
+  if (show === true) {
+    $("#survey_area").removeClass("hidden");
+  }
+  else {
+    $("#survey_area").addClass("hidden");
+  }
 }
 
 function show_survey(show) {
@@ -22,19 +22,19 @@ function add_survey_question(left_word, right_word, row_identifier) {
   row_identifier = row_identifier || "answer";
   new_answer.removeClass("hidden");
   new_answer.attr("question", left_word + "#" + right_word);
-  new_answer.find("input").on("click", function(event) {
+  new_answer.find("input").on("click", function (event) {
     var question = $(event.target).parents("tr").attr("question");
     var answer = $(event.target).attr("answer")
-    var response = JSON.stringify({question: question, answer: answer});
+    var response = JSON.stringify({ question: question, answer: answer });
 
     $.ajax({
-      url:"/survey_results",
-      type:"POST",
-      data:response,
-      contentType:"application/json; charset=utf-8",
+      url: "/survey_results",
+      type: "POST",
+      data: response,
+      contentType: "application/json; charset=utf-8",
       processData: false,
-      dataType:"json",
-      success: function(){
+      dataType: "json",
+      success: function () {
         console.log("Successfully sent data to server");
       }
     });
@@ -62,7 +62,7 @@ function fill_survey(answer_source) {
 }
 
 function fill_survey_partial(answer_source, count, offset) {
-  fill_survey(answer_source.slice(offset, offset+count));
+  fill_survey(answer_source.slice(offset, offset + count));
 }
 
 function proceed_survey() {
@@ -71,12 +71,12 @@ function proceed_survey() {
 }
 
 function resize_survey_content(width, height) {
-  $("#question_content").css({"width": width + "%", "height" : height + "%"});
+  $("#question_content").css({ "width": width + "%", "height": height + "%" });
 }
 
 function content_stage_2() {
   $(".title").text("Please help us improve by rating us according to the following criteria.");
-  $.getJSON( "/questions", function( data ) {
+  $.getJSON("/questions", function (data) {
     fill_survey(data.pragmatic_quality);
     //fill_survey_partial(data.hedonic_quality, 3, 0);
     show_survey_table(true);
@@ -87,17 +87,17 @@ function init_survey(div_name) {
   $("#" + div_name).html(survey_html_content());
 
   console.log("Init of survey tool");
-      setTimeout(function() {
-        show_survey(true);
-      }, 1000);
-  
-    $(".close-button").click(function() {
-        show_survey(false);
-    })
-  
-    $(".survey_reaction").click(function() {
-      proceed_survey();
-    });
+  setTimeout(function () {
+    show_survey(true);
+  }, 1000);
+
+  $(".close-button").click(function () {
+    show_survey(false);
+  })
+
+  $(".survey_reaction").click(function () {
+    proceed_survey();
+  });
 
 }
 
