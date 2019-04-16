d1d42feb494bd9795d822e24435ea27f6e2ef052
mandy.klingbeil <mandy.klingbeil@gmail.com>
2019-04-17 00:05:07 +0200

    allow questions to have multiple categories and varying amount of answers

--- index.js
+++ index.js
@@ -39,21 +39,121 @@ function table_exists(table, callback) {
   });
 }
 
+function add_question(type, parameters, callback) {
+  var question_stmt = db.prepare("INSERT INTO question (type) VALUES (?)");
+  // optional callback returns the ID of the newly created question
+
+  var question_id = -1;
+  var error = { "description": "unknown" };
+
+  question_stmt.run(type);
+  question_stmt.finalize();
+
+  var stmt = db.prepare("SELECT max(ID) as id FROM question");
+  stmt.all(function (err, rows) {
+    var question_id = rows[0].id;
+    //callback(question_id, callback_param);
+
+    for (var order = 0; order < parameters.responses.length; order++) {
+      var response = parameters.responses[order];
+      add_possible_reponse(question_id, response, order);
+    }
+    for (var category_index in parameters.categories) {
+      add_category_for_question(question_id, parameters.categories[category_index]);
+    }
+
+    if (callback != undefined) {
+      callback(question_id);
+    }
+  });
+
+}
+
+function add_category(name, callback) {
+  var category_stmt = db.prepare("INSERT INTO category (name) VALUES (?)");
+  // returns the ID of the newly created category in the callback
+
+  category_stmt.run(name);
+  category_stmt.finalize();
+
+  var stmt = db.prepare("SELECT max(ID) as id FROM category ");
+  db.serialize(function () {
+    stmt.all(function (err, rows) {
+      var category_id = rows[0].id;
+      console.log("Created category", category_id);
+      callback(category_id, {});
+    });
+  });
+}
+
+function add_possible_reponse(question_id, text, question_order) {
+  var possible_response_stmt = db.prepare("INSERT INTO possible_response (text, question_id, question_order) VALUES (?, ?, ?)");
+  possible_response_stmt.run(text, question_id, question_order);
+  possible_response_stmt.finalize();
+};
+
+function category_exists(category_name, callback) {
+  var stmt = db.prepare("SELECT id FROM category where name = ?");
+  stmt.all(category_name, function (err, rows) {
+    console.log("-->", category_name, rows);
+    var exists = (rows.length > 0);
+    var category_id = 0;
+    if (rows.length > 0) {
+      category_id = rows[0].id;
+    }
+
+    callback(exists, category_id);
+  });
+}
+
+function link_category_question(question_id, category_id) {
+  var stmt = db.prepare("INSERT INTO question_category (question_id, category_id) VALUES (?, ?)");
+  stmt.run(question_id, category_id);
+  stmt.finalize();
+}
+
+function add_category_for_question(question_id, category_name) {
+  category_exists(category_name, function (exists, category_id) {
+    console.log(category_name, exists ? "DOES" : "DOES NOT", "exist");
+
+    if (!exists) {
+      add_category(category_name, function (category_id) {
+        link_category_question(question_id, category_id);
+      });
+    }
+    else {
+      link_category_question(question_id, category_id);
+    }
+  });
+}
+
+function bootstrap_questions() {
+  categories = Object.keys(questions());
+  console.log(categories);
+  for (type_index in categories) {
+    var type = categories[type_index];
+    
+    for (var i in questions()[type]) {
+      var question_parameters = {
+        responses: questions()[type][i],
+        categories: [type, "blub"]
+      };
+      console.log(question_parameters);
+      add_question("two_type", question_parameters);
+    }
+  }
+}
+
 function init_questions() {
   table_exists("question", function (exists) {
     if (!exists) {
       db.serialize(function () {
-        db.run("CREATE TABLE IF NOT EXISTS question (id INTEGER PRIMARY KEY, option_left TEXT, option_right TEXT, category TEXT)");
+        db.run("CREATE TABLE IF NOT EXISTS question (id INTEGER PRIMARY KEY, type TEXT)");
+        db.run("CREATE TABLE IF NOT EXISTS possible_response (id INTEGER PRIMARY KEY, text TEXT, question_id INTEGER, question_order INTEGER)");
+        db.run("CREATE TABLE IF NOT EXISTS category (id INTEGER PRIMARY KEY, name TEXT)");
+        db.run("CREATE TABLE IF NOT EXISTS question_category (id INTEGER PRIMARY KEY, question_id INTEGER, category_id INTEGER)");
 
-        var stmt = db.prepare("INSERT INTO question (option_left, option_right, category) VALUES (?,?,?)");
-        for (var i in questions().hedonic_quality) {
-          stmt.run(questions().hedonic_quality[i][0], questions().hedonic_quality[i][1], "hedonic_quality");
-        }
-
-        for (var i in questions().pragmatic_quality) {
-          stmt.run(questions().pragmatic_quality[i][0], questions().pragmatic_quality[i][1], "pragmatic_quality");
-        }
-        stmt.finalize();
+        bootstrap_questions();
       });
     }
     else {
@@ -75,13 +175,37 @@ app.get('/admin/question', function (req, res) {
   }, undefined, limit, offset);
 });
 
+app.post('/admin/question', function (req, res, next) {
+  res.status(200);
+  res.json({ success: true });
+  db.serialize(function () {
+    var stmt = db.prepare("INSERT INTO question VALUES (?, ?)");
+    stmt.run(
+      + new Date(),
+      req.body.session_id
+    );
+    stmt.finalize();
+  });
+  console.log("(" + req.body.session_id + ") User opted out");
+});
+
+// Regular requests
+
 app.get("/session_id", function (req, res) {
   res.status(200);
   res.json({ session_id: uuidv1() });
 })
 
+function get_answers_for_question(question_id, callback) {
+  var stmt = db.prepare("SELECT text FROM possible_response where question_id = ? ORDER BY question_order ASC");
+
+  stmt.all(question_id, function (err, rows) {
+    callback(rows);
+  });
+}
+
 function get_questions(callback, category_name, limit, offset) {
-  console.log(limit, offset);
+  // select question.id as id, question.type as type, possible_response.text as text from question, possible_response where question.id = possible_response.question_id LIMIT ? OFFSET ? ORDER BY question_id ASC;
   if (category_name !== undefined) {
     var stmt = db.prepare("SELECT id, option_left, option_right FROM question WHERE category = ?");
     stmt.all(category_name, function (err, rows) {
@@ -89,9 +213,30 @@ function get_questions(callback, category_name, limit, offset) {
     });
   }
   else {
-    var stmt = db.prepare("SELECT * FROM question order by ID asc limit ? offset ?");
+
+    //SELECT * FROM possible_response INNER JOIN question ON question.id = (select id, type from question where question.id = possible_response.question_id order by question.id asc limit 5);
+    //var stmt = db.prepare("SELECT question.id AS id, question.type AS type, possible_response.text AS text FROM question, possible_response WHERE question.id = possible_response.question_id LIMIT ? OFFSET ? ORDER BY id ASC");
+
+    //var stmt = db.prepare("SELECT t1.id AS id, t1.type AS type, possible_response.text AS text FROM (SELECT * FROM question LIMIT ? OFFSET ?) t1, possible_response where t1.id = possible_response.question_id order by id asc, possible_response.question_order asc;");
+    var stmt = db.prepare("SELECT t1.id AS id, t1.type AS type, possible_response.text AS text, category.name as category FROM (SELECT * FROM question LIMIT ? OFFSET ?) t1, possible_response, question_category, category where t1.id = possible_response.question_id = question_category.question_id and question_category.category_id = category.id ORDER BY id ASC, possible_response.question_order ASC;");
     stmt.all(limit, offset, function (err, rows) {
-      callback(rows);
+      var result = [];
+      var mapping = {};
+      for (row_index in rows) {
+        var row = rows[row_index];
+        var key = "question_" + row.id;
+        if (!(key in mapping)) {
+          mapping[key] = result.length;
+          result.push({ "id": row.id, "type": row.type, "possible_reponses": [] , "categories" : []});
+        }
+        if (!result[mapping[key]].possible_reponses.includes(row.text)) {
+          result[mapping[key]].possible_reponses.push(row.text);
+        }
+        if (!result[mapping[key]].categories.includes(row.category)) {
+          result[mapping[key]].categories.push(row.category);
+        }
+      }
+      callback(result);
     });
   }
 }
